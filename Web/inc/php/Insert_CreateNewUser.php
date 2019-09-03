9ea7425bd999c996a8d076591f99e3c63e63deb7
Yunus Sobaci <yunussobaci@gmail.com>
2019-09-04 02:35:02 +0300

    BG

--- Web/inc/php/Insert_CreateNewUser.php
+++ Web/inc/php/Insert_CreateNewUser.php
@@ -11,7 +11,7 @@ $UserName = $_POST["UserName"];
 $Email = $_POST["Email"];
 $Password = $_POST["Password"];
 
-
+$NewAPIKey = generateRandomString().generateRandomString().generateRandomString().generateRandomString();
 
 $CheckHaveUserEmail = $Conn_pgsql->query("SELECT COUNT(*) FROM public.\"Users\" Where \"Email\"='$Email' LIMIT 1")->fetchAll(PDO::FETCH_ASSOC)[0]["count"];
 if (count($CheckHaveUserEmail) == 0)
@@ -22,15 +22,16 @@ if (count($CheckHaveUserEmail) == 0)
 
 $data08 = $Conn_pgsql->prepare("INSERT INTO public.\"Users\" (\"Name\", \"Surname\",\"Username\",\"Email\",\"Password\") VALUES ('$Name', '$Surname','$UserName','$Email','$Password')");
 $data08->execute();
+$UserId = $Conn_pgsql->lastInsertId();
 
-$data08 = $Conn_pgsql->prepare("INSERT INTO public.\"Users\" (\"Name\", \"Surname\",\"Username\",\"Email\",\"Password\") VALUES ('$Name', '$Surname','$UserName','$Email','$Password')");
+$data08 = $Conn_pgsql->prepare("INSERT INTO public.\"UserActivationCode\" (\"UserId\", \"Type\",\"Code\",\"Email\",\"Password\") VALUES ('$UserId '0','$NewAPIKey')");
 $data08->execute();
 
 include_once("Mail/Core.php");
 
 $file = file_get_contents('Mail/Template/MailActivationMail.html');
 
-$bodytag = str_replace("{{LINK}}", "https://willingly.tk/inc/php/Activation.php?Code".$Code, $file);
+$bodytag = str_replace("{{LINK}}", "https://willingly.tk/inc/php/Activation.php?Code=".$Code, $file);
 
 $MailActivationCore = new Core($Email,"Willingly Hesabinizi Dogrulayiniz",$bodytag);
 
@@ -38,3 +39,13 @@ print(json_encode(array(
                 "Status" => True,
                 "Message" => "Kullanici Olusturuldu"
                 )));
+
+function generateRandomString($length = 3) {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
--- Web/inc/php/LoginCheck.php
+++ Web/inc/php/LoginCheck.php
@@ -14,8 +14,6 @@ if (count($res) > 0) {
 		
     //foreach ($UserIdQuery as $item)
     $_SESSION['UserID'] =  $res[0]["Id"];
-			
-    
 
     print(json_encode(array('Status'=>true,"SessionID"=>session_id())));
 }else{
