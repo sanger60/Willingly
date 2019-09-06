8db5165e91e5477d05b12689a60bddf8feff9ca1
Yunus Sobaci <yunussobaci@gmail.com>
2019-09-06 21:37:32 +0300

    BG

--- Web/inc/php/Get_UserInformation.php
+++ Web/inc/php/Get_UserInformation.php
@@ -7,8 +7,6 @@ include_once("Config.php");
 
 $Response = Array();
 
-$Catagory = $Conn_pgsql->query("SELECT * FROM public.\"Catagory\"")->fetchAll(PDO::FETCH_ASSOC);
+$Users = $Conn_pgsql->query("SELECT * FROM public.\"Users\" where \"Id\"='$Global_UserID'")->fetchAll(PDO::FETCH_ASSOC);
 
-$Prce = $Conn_pgsql->query("SELECT * FROM public.\"Users\" where \"Id\"='$Global_UserID'")->fetchAll(PDO::FETCH_ASSOC);
-
-Print(json_encode(array("Catagory" =>$Catagory,"Price"=>$Prce)));
+Print(json_encode(array("Data" =>$Users)));
