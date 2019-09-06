4cc369a7fa908b36293ea03d01fc8b19564b7d75
Ersan Terzi <38361156+sanger60@users.noreply.github.com>
2019-09-06 03:40:12 +0300

    BM

--- Web/assets/js/custom/custom.js
+++ Web/assets/js/custom/custom.js
@@ -107,17 +107,30 @@ function SearchBtn(){
     if(keyType == "Willingers"){
         catKey= 1;
     }
-    else if(keyType == "MeetUp"){
+    else if(keyType == "Jobs"){
         catKey= 2;
     }
 
-    if(key != "")
+    if(catKey == 1)
     {
-        window.location.href="userlisting.php/?search="+key+"&searchType="+catKey+"&page="+page;
-    }
-    else{
-        window.location.href="userlisting.php/?searchType="+catKey+"&page="+page;
+        if(key != "")
+        {
+            window.location.href="userlisting.php/?search="+key+"&searchType="+catKey+"&page="+page;
+        }
+        else{
+            window.location.href="userlisting.php/?searchType="+catKey+"&page="+page;
 
+        }
+    }
+    else if(catKey == 2){
+        if(key != "")
+        {
+            window.location.href="joblisting.php/?search="+key+"&searchType="+catKey+"&page="+page;
+        }
+        else{
+            window.location.href="joblisting.php/?searchType="+catKey+"&page="+page;
+
+        }
     }
 }
 // img,user,ilan başlık,ilan parası,ülke,ilan açıklama,ilan rate,ilan skills,ilan feedback
@@ -135,18 +148,35 @@ if(window.location.toString().includes("http://willingly.com/userlisting.php"))
 // });
 var ItemsVue;
 function FreeLancerList(){
-
      var urlParams = new URLSearchParams(location.search);
      var searchKey = urlParams.get('search');
      var searchType2 = urlParams.get('searchType');
      var page = urlParams.get('page');
      var category = urlParams.get("category");
      var price = urlParams.get("price");
-        // created(){
-        //     fetch("./assets/data.json")
-        //     .then((res) => {return res.json(); })
-        //     .then((res) => { items = res; this.items = items; })
-        // },
+
+    if(category != null && category != ""){
+
+        var ul = $('#filtertags');
+        var li = ul.find('li:first').clone(true).appendTo(ul);
+
+        li.find('a span')[0].innerText = "Kategori = "+category;
+    }
+
+    if(price != null && price != ""){
+        var ul = $('#filtertags');
+        var li = ul.find('li:first').clone(true).appendTo(ul);
+
+        li.find('a span')[0].innerText = "Fiyat = "+price;
+    }
+
+        
+     var ul2 = $('#filtertags');
+     var liCount = ul2.find('li');
+     if(liCount.length > 1){
+         ul2.find('li:first').find('a span')[0].innerText = "Filtreler = ";
+     }
+
      ItemsVue = new Vue({
         el: '#mainVue',
         data:{
@@ -223,10 +253,30 @@ function FreeLancerCategoryFilter(a,b){
         }
     }
 }
+function updateQueryStringParam(param, value) {
+    baseUrl = [location.protocol, '//', location.host, location.pathname].join('');
+    urlQueryString = document.location.search;
+    var newParam = key + '=' + value,
+    params = '?' + newParam;
+  
+    // If the "search" string exists, then build params from it
+    if (urlQueryString) {
+      keyRegex = new RegExp('([\?&])' + key + '[^&]*');
+      // If param exists already, update it
+      if (urlQueryString.match(keyRegex) !== null) {
+        params = urlQueryString.replace(keyRegex, "$1" + newParam);
+      } else { // Otherwise, add it to end of query string
+        params = urlQueryString + '&' + newParam;
+      }
+    }
+    window.history.replaceState({}, "", baseUrl + params);
+  }
 
 function FreeLancerFilter(){
-    var filterProp = document.getElementsByName("category");
+    var filterProp = $('input[name=category]:checked');
     var filterCheckedProps = "";
+
+    console.log(filterProp);
 
     for(var i=0;i< filterProp.length;i++){
         if(filterProp[i].checked == true)
@@ -248,12 +298,36 @@ function FreeLancerFilter(){
 
     var urlParams = new URLSearchParams(location.search);
     var catKey = urlParams.get('category');
-
-    if(catKey != null && catKey != "")
+    var priceKey = urlParams.get('price');
+    if(catKey != null && catKey != "" && priceKey != null && priceKey != "")
     {
+        var locate = window.location.href;
+        console.log("link = "+locate);
         if(filterCheckedProps != "")
         {
-            urlParams.set("category",filterCheckedProps);
+            locate = locate.replace(catKey,filterCheckedProps);
+        }
+
+        if(PriceTemplate != "")
+        {
+            locate = locate.replace(priceKey,PriceTemplate);
+        }
+
+        window.location.href = locate;
+    }
+    else if(catKey != null && catKey != ""){
+        if(filterCheckedProps != "")
+        {
+            locate = locate.replace(catKey,filterCheckedProps);
+
+            window.location.href = locate;
+        }
+    }
+    else if(priceKey != null && priceKey != ""){
+        if(PriceTemplate != ""){
+            locate = locate.replace(priceKey,PriceTemplate);
+
+            window.location.href = locate;
         }
     }
     else{
@@ -279,8 +353,263 @@ function IndexSubmit(){
 
 function updateTextInput(val) {
     document.getElementById('minOutput').innerText=val+" ₺"; 
+    document.getElementById('max-range').setAttribute("min",val.toString()); 
   }
 
 function updateTextInput2(val) {
     document.getElementById('maxOutput').innerText=val+" ₺"; 
   }
+
+  // ---------------------------------------------------------------------------------- \\
+                        //          USER    DETAİLS     PAGE            \\
+
+function AdDetail(item){
+    var id = item.getAttribute("title");
+
+    window.location.href="http://willingly.com/usersingle.php?uniqorne="+id;
+}
+
+if(window.location.toString().includes("http://willingly.com/usersingle.php"))
+{
+    var urlParams = new URLSearchParams(location.search);
+    var advId = urlParams.get('uniqorne');
+    
+    var AdDetailVue = new Vue({
+        el: '#wt-main',
+        data:{
+            item = []
+        },
+        mounted: function(){
+            var self = this;
+            $.ajax({
+                url:"http://willingly.tk/inc/php/Get_FreelanceAdvertisement.php",
+                method: "POST",
+                data: {Id:advId},
+                dataType: "JSON",
+                success:function(data) {
+                    self.item = data;
+                },
+                error:function(a,b,g) {
+                    Swal.fire("Bir hata oluştu !");
+                }
+            });
+        }
+    });
+
+}
+
+function FreelancerSendOffer(){
+    var offer = document.getElementById("#txtOffer").value;
+    var id = 1;
+
+    $.ajax({
+        url:"",
+        method:"POST",
+        data:{Offer:offer,AdId:id},
+        dataType:"JSON",
+        success:function(data){
+            if(data.Status == true){
+                Swal.fire("Teklifiniz Gönderildi");
+            }
+            else{
+                Swal.fire("Teklifiniz Gönderilemedi");
+            }
+        },
+        error:function(a,b,g){
+            Swal.fire("Teklifiniz Gönderilemedi");
+        }
+    });
+}
+
+  // ---------------------------------------------------------------------------------- \\
+                        //          JOB    LİST     PAGE            \\
+
+    function JobList(){
+        var urlParams = new URLSearchParams(location.search);
+        var searchKey = urlParams.get('search');
+        var searchType2 = urlParams.get('searchType');
+        var page = urlParams.get('page');
+        var category = urlParams.get("category");
+        var price = urlParams.get("price");
+    
+        if(category != null && category != ""){
+    
+            var ul = $('#filtertags');
+            var li = ul.find('li:first').clone(true).appendTo(ul);
+    
+            li.find('a span')[0].innerText = "Kategori = "+category;
+        }
+    
+        if(price != null && price != ""){
+            var ul = $('#filtertags');
+            var li = ul.find('li:first').clone(true).appendTo(ul);
+    
+            li.find('a span')[0].innerText = "Fiyat = "+price;
+        }
+    
+            
+        var ul2 = $('#filtertags');
+        var liCount = ul2.find('li');
+        if(liCount.length > 1){
+            ul2.find('li:first').find('a span')[0].innerText = "Filtreler = ";
+        }
+    
+        var ItemsVue = new Vue({
+            el: '#wt-main',
+            data:{
+                categories: [],
+                MinPrice: 0,
+                MaxPrice: 0,
+                items: []
+            },
+            mounted: function () {
+                var self = this;
+                $.ajax({
+                    url: 'http://willingly.tk/inc/php/Get_JobAdvertisement.php',
+                    method: 'POST',
+                    data: {searchKey:searchKey,searchtip: searchType2,PageId:page,category: category,price:price},
+                    dataType: "JSON",
+                    success: function (data) {
+                        self.items = data;
+                    },
+                    error: function (error) {
+                        Swal.fire({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: 'Bir şeyler yanlış gitti!'
+                        });
+                    }
+                });
+    
+                $.ajax({
+                    url: 'http://willingly.tk/inc/php/Get_FilterConfig.php',
+                    method: 'POST',
+                    dataType: "JSON",
+                    timeout: 60000,
+                    success: function (data) {
+                        self.categories = data.Catagory;
+                        self.MinPrice = data.Price[0].min;
+                        self.MaxPrice = data.Price[0].max;
+                    },
+                    error: function (error) {
+                        Swal.fire({
+                            type: 'error',
+                            title: 'Oops...',
+                            text: 'Bir şeyler yanlış gitti!'
+                        });
+                    }
+                });
+            }
+        });
+    
+        window.vue = ItemsVue;
+    
+    }
+
+    function JobPageChanged(a){
+    
+        var PageId = parseInt(a.value);
+        console.log("Basılan page ID = "+PageId);
+        console.log("Şuanki Location = "+location);
+        if(location.toString().includes('search') || location.toString().includes('searchType'))
+        {
+            console.log("ilk if");
+            var urlParams = new URLSearchParams(location.search);
+            var searchKey = urlParams.get('search');
+            var searchType2 = urlParams.get('searchType');
+            window.location.href = "http://willingly.com/joblisting.php/?search="+searchKey+"&searchType="+searchType2+"&page="+PageId;
+        }
+        else if(location.toString().includes('category')){
+            console.log("ikinci if");
+    
+            var urlParams = new URLSearchParams(location.search);
+            var catKey = urlParams.get('category');
+            window.location.href = "http://willingly.com/joblisting.php/?category="+catKey+"&page="+PageId;
+    
+        }
+        else{
+            console.log("son deger girdi");
+    
+            window.location.href = "http://willingly.com/joblisting.php/?page="+PageId;
+        }
+    
+       
+    }
+
+    function JobFilter(){
+        var filterProp = $('input[name=category]:checked');
+        var filterCheckedProps = "";
+    
+        console.log(filterProp);
+    
+        for(var i=0;i< filterProp.length;i++){
+            if(filterProp[i].checked == true)
+            {
+                filterCheckedProps += filterProp[i].value;
+    
+                if(i != (filterProp.length-1))
+                {
+                    filterCheckedProps += ",";
+                }
+            }
+        }
+    
+        console.log("Categori Filtre Post Sorgusu : "+filterCheckedProps);
+        var PriceTemplate = document.getElementById("wt-consultationfeeamount").value;
+        
+    
+        var urlParams = new URLSearchParams(location.search);
+        var catKey = urlParams.get('category');
+        var priceKey = urlParams.get('price');
+        if(catKey != null && catKey != "" && priceKey != null && priceKey != "")
+        {
+            var locate = window.location.href;
+            console.log("link = "+locate);
+            if(filterCheckedProps != "")
+            {
+                locate = locate.replace(catKey,filterCheckedProps);
+            }
+    
+            if(PriceTemplate != "")
+            {
+                locate = locate.replace(priceKey,PriceTemplate);
+            }
+    
+            window.location.href = locate;
+        }
+        else if(catKey != null && catKey != ""){
+            if(filterCheckedProps != "")
+            {
+                locate = locate.replace(catKey,filterCheckedProps);
+    
+                window.location.href = locate;
+            }
+        }
+        else if(priceKey != null && priceKey != ""){
+            if(PriceTemplate != ""){
+                locate = locate.replace(priceKey,PriceTemplate);
+    
+                window.location.href = locate;
+            }
+        }
+        else{
+            if(filterCheckedProps != "" && PriceTemplate != "")
+            {
+                window.location.href += "&price="+PriceTemplate+"&category="+filterCheckedProps;
+        
+            }
+            else if(filterCheckedProps != ""){
+                window.location.href += "&category="+filterCheckedProps;
+        
+            }
+            else if(PriceTemplate != ""){
+                window.location.href += "&price="+PriceTemplate;
+            }
+        }
+    }
+    
+    function JobDetail(item){
+        var id = item.getAttribute("title");
+
+        window.location.href="http://willingly.com/jobsingle.php?uniqorne="+id;
+    }
---
+++ Web/joblisting.php
@@ -0,0 +1,134 @@
+<?php require 'inc/frontend/views/head_start.php'; ?>
+<?php require 'inc/frontend/views/head_end.php'; ?>
+
+<main id="wt-main" class="wt-main wt-haslayout wt-innerbgcolor">
+				<div class="wt-haslayout wt-main-section">
+					<!-- User Listing Start-->
+					<div class="wt-haslayout">
+						<div class="container">
+							<div class="row">
+								<div id="wt-twocolumns" class="wt-twocolumns wt-haslayout">
+									<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 col-xl-4 float-left">
+										<aside id="wt-sidebar" class="wt-sidebar">
+                                            <div class="wt-widget wt-effectiveholder">
+													<div class="wt-widgettitle">
+														<h2>Kategoriler</h2>
+													</div>
+													<div class="wt-widgetcontent">
+														<form class="wt-formtheme wt-formsearch">
+															<fieldset>
+																<div class="form-group">
+																	<input type="text" name="Search" onkeydown="FreeLancerCategoryFilter(this,'category');" onchange="FreeLancerCategoryFilter(this,'category');" class="form-control" placeholder="Kategori Ara">
+																</div>
+															</fieldset>
+															<fieldset>
+																<div class="wt-checkboxholder wt-verticalscrollbar">
+																	<span class="wt-checkbox" v-for="item in categories">
+																		<input v-bind:id="item.Name" type="checkbox" name="category" v-bind:value="item.Id" checked="true">
+																		<label v-bind:for="item.Name"> {{item.Name}}</label>
+																	</span>
+																</div>
+															</fieldset>
+														</form>
+													</div>
+                                            </div>
+											<div class="wt-widget wt-effectiveholder">
+												<div class="wt-widgettitle">
+													<h2>Fiyat Ölçeklendirme</h2>
+												</div>
+												<div class="wt-widgetcontent">
+													<form class="wt-formtheme wt-formsearch">
+														<fieldset>
+															<div class="wt-checkboxholder">
+																<div id="wt-productrangeslider" class="wt-productrangeslider wt-themerangeslider"></div>
+																<div class="wt-amountbox">
+																	<input type="text" id="wt-consultationfeeamount" readonly="">
+																</div>
+															</div>
+														</fieldset>
+													</form>
+												</div>
+											</div>
+                                            <div class="wt-widget wt-applyfilters-holder">
+													<div class="wt-widgetcontent">
+														<div class="wt-applyfilters">
+															<span>En son uygulamak için “Filtre Uygula” ya tıklayın <br> Sizin tarafınızdan yapılan değişiklikler.</span>
+																<input type="submit" class="wt-btn" id="btnFilter" onclick="FreeLancerFilter();" />
+														</div>
+													</div>
+											</div>
+										</aside>
+									</div>
+									<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-8 float-left">
+										<div class="wt-userlistingholder wt-haslayout">
+											<div class="wt-userlistingtitle">
+                                                 <span>01 - 48 / 57143 <em>"Kategori Adı"</em> için sonuçlar</span>
+											</div>
+                                            <div class="wt-filterholder">
+													<ul class="wt-filtertag" id="filtertags">
+														<li class="alert alert-dismissable fade in">
+															<a href="javascrip:void(0)"><span></span></a>
+														</li>
+													</ul>
+											</div>
+											<div class="wt-userlistinghold wt-featured wt-userlistingholdvtwo" v-for="item in items.Data">
+												<span class="wt-featuredtag"><img src="/../../../assets/images/featured.png" alt="img description" data-tipso="Plus Member" class="template-content tipso_style"></span>
+												<div class="wt-userlistingcontent">
+													<div class="wt-contenthead">
+														<div class="wt-title">
+															<a href="usersingle.html"><i class="fa fa-check-circle"></i> {{item.CompanyName}}</a>
+															<a href="jobsingle.html"><h2>{{item.Tittle}}</h2></a>
+														</div>
+														<div class="wt-description">
+															<p>{{item.Description}}</p>
+														</div>
+														<div class="wt-tag wt-widgettag">
+															<a href="javascript:void(0);">PHP</a>
+															<a href="javascript:void(0);">HTML</a>
+															<a href="javascript:void(0);">JQuery</a>
+														</div>
+													</div>
+													<div class="wt-viewjobholder">
+														<ul>
+															<li><span><i class="fa fa-dollar-sign wt-viewjobdollar"></i></span></li> <!-- Orta düzy yatırım - yüksek yatırım - düşük yatırım -->
+															<li><span><i class="far fa-clock wt-viewjobclock"></i>Zaman: {{item.time}}</span></li>
+															<li><span><i class="fa fa-tag wt-viewjobtag"></i>İş No: {{item.Id}}</span></li> <!-- gy3yV2Vm5u -->
+															<li class="wt-btnarea"><a href="userlisting.html" class="wt-btn">Detay Göster</a></li>
+														</ul>
+													</div>
+												</div>
+											</div>
+                                            <nav class="wt-pagination">
+													<ul>
+														<li class="wt-prevpage"><a href="javascrip:void(0);"><i class="lnr lnr-chevron-left"></i></a></li>
+														<li>
+															<input type="submit" class="paginationBtn" onclick="pageChanged(this);" value="1" />
+														</li>
+														<li>
+															<input type="submit" class="paginationBtn" onclick="pageChanged(this);" value="2" />
+														</li>
+														<li>
+															<input type="submit" class="paginationBtn" onclick="pageChanged(this);" value="3" />
+														</li>
+														<li>
+															<input type="submit" class="paginationBtn" onclick="pageChanged(this);" value="4" />
+														</li>
+														<li><a href="javascrip:void(0);">...</a></li>
+														<li>
+															<input type="submit" class="paginationBtn" onclick="pageChanged(this);" value="50" />
+														</li>
+														<li class="wt-nextpage"><a href="javascrip:void(0);"><i class="lnr lnr-chevron-right"></i></a></li>
+													</ul>
+												</nav>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- User Listing End-->
+				</div>
+			</main>
+
+<?php require 'inc/frontend/views/footer_start.php'; ?>
+<?php require 'inc/frontend/views/footer_end.php'; ?>
