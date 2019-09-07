707614bab3b66e3d895d8d7ca5bc189398a02cbd
Ersan Terzi <38361156+sanger60@users.noreply.github.com>
2019-09-07 23:31:52 +0300

    Test

--- Web/messages.php
+++ Web/messages.php
@@ -4,130 +4,117 @@
 <?php require 'inc/frontend/views/head_end.php'; ?>
 
 <main id="wt-main" class="wt-main wt-haslayout">
-				<!--Sidebar Start-->
-				<div id="wt-sidebarwrapper" class="wt-sidebarwrapper">
-					<div id="wt-btnmenutoggle" class="wt-btnmenutoggle">
-						<span class="menu-icon">
-							<em></em>
-							<em></em>
-							<em></em>
-						</span>
-					</div>
-					<div id="wt-verticalscrollbar" class="wt-verticalscrollbar">
-						<div class="wt-companysdetails wt-usersidebar">
-							<figure class="wt-companysimg">
-								<img src="/../../../assets/images/sidebar/img-01.jpg" alt="img description">
-							</figure>
-							<div class="wt-companysinfo">
-								<figure><img src="/../../../assets/images/sidebar/img-01.jpg" alt="img description"></figure>
-								<div class="wt-title">
-									<h2><a href="javascript:void(0);"> Louanne Mattioli</a></h2>
-									<span>Amento Tech</span>
-								</div>
-								<div class="wt-btnarea"><a href="dashboard-postjob.html" class="wt-btn">Post a Job</a></div>
-							</div>
-						</div>
-						<nav id="wt-navdashboard" class="wt-navdashboard">
-							<ul>
-								<li class="menu-item-has-children">
-									<a href="javascript:void(0);">
-										<i class="ti-dashboard"></i>
-										<span>Insights</span>
-									</a>
-									<ul class="sub-menu">
-										<li><hr><a href="dashboard-insights.html">Insights</a></li>
-										<li><hr><a href="dashboard-insightsuser.html">Insights User</a></li>
-									</ul>
-								</li>
-								<li>
-									<a href="dashboard-profile.html">
-										<i class="ti-briefcase"></i>
-										<span>My Profile</span>
-									</a>
-								</li>
-								<li class="menu-item-has-children">
-									<a href="javascript:void(0);">
-										<i class="ti-package"></i>
-										<span>All Jobs</span>
-									</a>
-									<ul class="sub-menu">
-										<li><hr><a href="dashboard-completejobs.html">Completed Jobs</a></li>
-										<li><hr><a href="dashboard-canceljobs.html">Cancelled Jobs</a></li>
-										<li><hr><a href="dashboard-ongoingjob.html">Ongoing Jobs</a></li>
-										<li><hr><a href="dashboard-ongoingsingle.html">Ongoing Single</a></li>
-									</ul>
-								</li>
-								<li>
-									<a href="dashboard-managejobs.html">
-										<i class="ti-announcement"></i>
-										<span>Manage Jobs</span>
-									</a>
-								</li>
-								<li class="wt-notificationicon menu-item-has-children wt-active">
-									<a href="javascript:void(0);">
-										<i class="ti-pencil-alt"></i>
-										<span>Messages</span>
-									</a>
-									<ul class="sub-menu">
-										<li class="wt-active"><hr><a href="dashboard-messages.html">Messages</a></li>
-										<li><hr><a href="dashboard-messages2.html">Messages V 2</a></li>
-									</ul>
-								</li>
-								<li>
-									<a href="dashboard-saveitems.html">
-										<i class="ti-heart"></i>
-										<span>My Saved Items</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-invocies.html">
-										<i class="ti-file"></i>
-										<span>Invoices</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-category.html">
-										<i class="ti-layers"></i>
-										<span>Category</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-packages.html">
-										<i class="ti-money"></i>
-										<span>Packages</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-proposals.html">
-										<i class="ti-bookmark-alt"></i>
-										<span>Proposals</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-accountsettings.html">
-										<i class="ti-anchor"></i>
-										<span>Account Settings</span>
-									</a>
-								</li>
-								<li>
-									<a href="dashboard-helpsupport.html">
-										<i class="ti-tag"></i>
-										<span>Help &amp; Support</span>
-									</a>
-								</li>
-								<li>
-									<a href="index.html">
-										<i class="ti-shift-right"></i>
-										<span>Logout</span>
-									</a>
-								</li>
-							</ul>
-						</nav>
-						<div class="wt-navdashboard-footer">
-							<span>Worktern. © 2019 All Rights Reserved.</span>
-						</div>
-					</div>
-				</div>
+<div id="wt-sidebarwrapper" class="wt-sidebarwrapper">
+                <div id="wt-btnmenutoggle" class="wt-btnmenutoggle">
+                    <span class="menu-icon">
+                        <em></em>
+                        <em></em>
+                        <em></em>
+                    </span>
+                </div>
+                <div id="wt-verticalscrollbar" class="wt-verticalscrollbar">
+                    <div class="wt-companysdetails wt-usersidebar">
+                        <figure class="wt-companysimg">
+                            <img src="/../../../assets/images/sidebar/img-01.jpg" alt="img description">
+                        </figure>
+                        <div class="wt-companysinfo">
+                            <figure><img src="http://amentotech.com/htmls/worktern/images/sidebar/img-02.jpg" alt="img description"></figure>
+                            <div class="wt-title">
+                                <h2><a href="javascript:void(0);">{{items.Name}} {{items.Surname}}</a></h2>
+                                <span>{{items.Username}}</span>
+                            </div>
+                            <div class="wt-btnarea"><a href="dashboard-postjob.html" class="wt-btn">Bir İş İlanı Paylaş</a></div>
+                        </div>
+                    </div>
+                    <nav id="wt-navdashboard" class="wt-navdashboard">
+                        <ul>
+                            <li class="wt-active">
+                                <a href="dashboard-profile.html">
+                                    <i class="ti-briefcase"></i>
+                                    <span>Profi</span>
+                                </a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="javascript:void(0);">
+                                    <i class="ti-package"></i>
+                                    <span>İşler</span>
+                                </a>
+                                <ul class="sub-menu">
+                                    <li><hr><a href="dashboard-completejobs.html">Biten İşler</a></li>
+                                    <li><hr><a href="dashboard-ongoingjob.html">Aldığım İşler</a></li> <!-- dashboard-ongoingsingle.html -->
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="dashboard-managejobs.html">
+                                    <i class="ti-announcement"></i>
+                                    <span>Manage Jobs</span>
+                                </a>
+                            </li>
+                            <li class="wt-notificationicon menu-item-has-children">
+                                <a href="javascript:void(0);">
+                                    <i class="ti-pencil-alt"></i>
+                                    <span>Messages</span>
+                                </a>
+                                <ul class="sub-menu">
+                                    <li><hr><a href="messages.php">Messages</a></li>
+                                    <li><hr><a href="dashboard-messages2.html">Messages V 2</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="dashboard-saveitems.html">
+                                    <i class="ti-heart"></i>
+                                    <span>My Saved Items</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-invocies.html">
+                                    <i class="ti-file"></i>
+                                    <span>Invoices</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-category.html">
+                                    <i class="ti-layers"></i>
+                                    <span>Category</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-packages.html">
+                                    <i class="ti-money"></i>
+                                    <span>Packages</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-proposals.html">
+                                    <i class="ti-bookmark-alt"></i>
+                                    <span>Proposals</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-accountsettings.html">
+                                    <i class="ti-anchor"></i>
+                                    <span>Account Settings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="dashboard-helpsupport.html">
+                                    <i class="ti-tag"></i>
+                                    <span>Help &amp; Support</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="index.html">
+                                    <i class="ti-shift-right"></i>
+                                    <span>Çıkış Yap</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="wt-navdashboard-footer">
+                        <span>Willingly. © 2019 Bütün Hakları Saklıdır.</span>
+                    </div>
+                </div>
+            </div>
 				<!--Sidebar Start-->
 				<!--Register Form Start-->
 				<section class="wt-haslayout wt-dbsectionspace">
